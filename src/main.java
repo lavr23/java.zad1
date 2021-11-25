fd1e2f237f16eacbcee6514e58157b50453a004f
Vladimir Lavrikov <vlavrikov83@gmail.com>
2021-11-25 17:35:34 +0300

    sum

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
-
+ int balance = 2000000000;
+ int transfer = 500000000;
+ int sum = balance + transfer;
+        System.out.println(sum);
     }
 }
